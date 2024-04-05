5e4bb42fb45eef63e792fc97971650eef5cbd814
hossam <hossameddine.djennad@gmail.com>
2024-04-06 00:16:57 +0200

    category index

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Admin\CategoryController;
 
 
 
 Route::get('/', [\App\Http\Controllers\StoreController::class,'index']);
 
 Route::resource('products', ProductController::class);
+Route::resource('categories', CategoryController::class);
