4e5f2f18b794e050cf3f50f27f7225feaafdb6bb
hossam <hossameddine.djennad@gmail.com>
2024-04-07 01:06:40 +0200

    add breeze packege

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\Admin\CategoryController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/', [\App\Http\Controllers\StoreController::class,'index']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::resource('products', ProductController::class);
-Route::resource('categories', CategoryController::class);
+require __DIR__.'/auth.php';
