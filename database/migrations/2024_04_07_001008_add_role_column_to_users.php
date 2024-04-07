fe7477b359ac65cd1b96482b7bc1ea6f23ad32b8
hossam <hossameddine.djennad@gmail.com>
2024-04-07 02:36:00 +0200

    add Middleware

--- database/migrations/2024_04_07_001008_add_role_column_to_users.php
+++ database/migrations/2024_04_07_001008_add_role_column_to_users.php
@@ -13,6 +13,7 @@ return new  class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->enum('role',['user','editor','admin']);
         });
     }
 
@@ -23,6 +24,7 @@ return new  class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->dropColumn('role');
         });
     }
 };
